1958af40af25c0ed4800f3a77937531f1bd64b31
marua <meb@mettrr.com>
2019-11-01 16:36:20 +0000

    testing this starter

---
+++ src/pages/test.js
@@ -0,0 +1,13 @@
+import React from "react"
+import Layout from "../components/layout"
+
+export default () => (
+	<Layout>
+		<div style={{ color: 'teal' }}>
+			<h1> Is about playing around with Gatsby</h1>
+			<p>
+				So much fun. Ole ole
+			</p>
+		</div>
+	</Layout>
+)
